954377ea7802cff5bb4e6feb56599bed283a6388
İlayda Nur <157166031+ilaydanur-ucar@users.noreply.github.com>
2026-03-03 01:54:50 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Ilayda Nur Ucar</p>
+      <p>Ogrenci No: 235541054</p>
+    </div>
+  )
+}
+
+export default App
